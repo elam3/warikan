c9f860cc58860146b6fca2b66b43e1572063a694
Edison Lam <elam3@edisons-air.attlocal.net>
2016-12-10 10:18:51 -0800

    Initial Commit

---
+++ warikanTests/warikanTests.swift
@@ -0,0 +1,36 @@
+//
+//  warikanTests.swift
+//  warikanTests
+//
+//  Created by Edison Lam on 12/10/16.
+//  Copyright © 2016 Edison Lam. All rights reserved.
+//
+
+import XCTest
+@testable import warikan
+
+class warikanTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
