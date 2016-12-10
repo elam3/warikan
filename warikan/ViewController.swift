52d7048a7fd6e6771e093dc304245ad2c8c265d2
Edison Lam <elam3@Edisons-Air.attlocal.net>
2016-12-10 11:54:53 -0800

    bare bones

--- warikan/ViewController.swift
+++ warikan/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipSegCtrl: UISegmentedControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +27,24 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+
+    @IBAction func tipSegCtrlListener(_ sender: Any) {
+        calculateTip(sender)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        let tipPercentages = [0.15, 0.20, 0.25]
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipSegCtrl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    
 }
 
